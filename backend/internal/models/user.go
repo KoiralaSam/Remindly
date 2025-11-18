4d4c8bd69f1cf07a4c35aa98423969a0b3bc219c
Samarpan Koirala <samarpankoirala@gmail.com>
2025-11-18 15:39:20 -0600

    changed backend structure. Connected to db using pgx, setup migrations using goose

---
+++ backend/internal/models/user.go
@@ -0,0 +1,10 @@
+package models
+
+//example code
+// type User struct {
+// 	ID        string `json:"id"`
+// 	Email     string `json:"email"`
+// 	Password  string `json:"password"`
+// 	CreatedAt time.Time `json:"created_at"`
+// 	UpdatedAt time.Time `json:"updated_at"`
+// }
