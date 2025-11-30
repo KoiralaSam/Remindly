d05e04cda3e38c9f786f7f73920c5b4f23880919
adk-saugat <adhikarisaugat34@gmail.com>
2025-11-30 13:22:52 -0600

    made group to use existing createdBy userId

--- backend/internal/routes/routes.go
+++ backend/internal/routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"github.com/KoiralaSam/Remindly/backend/internal/handlers"
+	"github.com/KoiralaSam/Remindly/backend/internal/middleware"
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,5 +10,8 @@ func SetupRoutes(server *gin.Engine) {
 	server.POST("/api/auth/register", handlers.RegisterUser)
 	server.POST("/api/auth/login", handlers.Login)
 
-	server.POST("/api/groups", handlers.CreateGroup)
+	authenticated := server.Group("/api")
+	authenticated.Use(middleware.AuthMiddleware)
+
+	authenticated.POST("/groups", handlers.CreateGroup)
 }
