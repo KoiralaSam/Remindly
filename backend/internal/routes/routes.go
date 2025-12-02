05a1a68cbb02f3e781d1054e654479f92482f60c
adk-saugat <adhikarisaugat34@gmail.com>
2025-12-01 20:12:48 -0600

    added getGroupById for the members

--- backend/internal/routes/routes.go
+++ backend/internal/routes/routes.go
@@ -7,22 +7,33 @@ import (
 )
 
 func SetupRoutes(server *gin.Engine) {
+	// Authentication Routes
 	server.POST("/api/auth/register", handlers.RegisterUser)
 	server.POST("/api/auth/login", handlers.Login)
 
 	authenticated := server.Group("/api")
 	authenticated.Use(middleware.AuthMiddleware)
 
-	authenticated.POST("/groups", handlers.CreateGroup)
 	authenticated.GET("/logout", handlers.Logout)
+
+	// User Routes
 	authenticated.GET("/users/me", handlers.GetUser)
 	authenticated.PATCH("/users/me", handlers.UpdateUser)
+
+	// Group Routes
+	authenticated.POST("/groups", handlers.CreateGroup)
+	authenticated.GET("/groups", handlers.GetGroups)
 
 	authenticatedGroupMember := authenticated.Group("/groups/:groupID")
 	authenticatedGroupMember.Use(middleware.AuthGroupMemberMiddleware)
 
+	authenticatedGroupMember.GET("", handlers.GetGroupByID)
+	authenticatedGroupMember.PATCH("", handlers.UpdateGroup)
+	authenticatedGroupMember.DELETE("", handlers.DeleteGroup)
+
+	// Group Member Routes
 	authenticatedGroupMember.POST("/members", handlers.AddGroupMember)
 	authenticatedGroupMember.GET("/members", handlers.GetGroupMembers)
 	authenticatedGroupMember.PATCH("members/:userId", handlers.UpdateGroupMemberRole)
-	//authenticatedGroupMember.DELETE("members/:userId", handlers.DeleteGroupMember)
+	authenticatedGroupMember.DELETE("members/:userId", handlers.DeleteGroupMember)
 }
