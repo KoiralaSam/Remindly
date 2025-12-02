83130a673a13dcb0a2b6dea3638ce37ac3ffc8d0
adk-saugat <adhikarisaugat34@gmail.com>
2025-12-01 20:46:12 -0600

    added owner to group-member when group created

--- backend/migrations/20251201043540_add_group_members_table.sql
+++ backend/migrations/20251201043540_add_group_members_table.sql
@@ -5,12 +5,9 @@ CREATE TABLE group_members (
     group_id UUID NOT NULL REFERENCES groups(id) ON DELETE CASCADE,
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     role    TEXT NOT NULL REFERENCES group_roles(role) ON DELETE CASCADE,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
-
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     UNIQUE (group_id, user_id)
-)
-
-
+);
 -- +goose StatementEnd
 
 -- +goose Down
